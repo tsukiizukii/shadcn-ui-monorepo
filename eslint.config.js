52d7df570442203c84031b3dce7b6dbf616a0098
vakasurvivor <110611464+vakasurvivor@users.noreply.github.com>
2026-01-26 06:22:03 +0900

    変更

--- eslint.config.js
+++ eslint.config.js
@@ -1,38 +1,9 @@
-// // This configuration only applies to the package manager root.
-// /** @type {import("eslint").Linter.Config} */
-// module.exports = {
-//   ignorePatterns: ["apps/**", "packages/**"],
-//   extends: ["@workspace/eslint-config/library.js"],
-//   parser: "@typescript-eslint/parser",
-//   parserOptions: {
-//     project: true,
-//   },
-// };
-
-// eslint.config.js
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import tseslint from "typescript-eslint";
-
-// workspace の共有 config（flat 化されている前提）
-// import workspaceLibrary from "@workspace/eslint-config/library.js";
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+import { config as baseConfig } from "./packages/eslint-config/base.js";
 
 export default [
-  // === ルートのみ適用 ===
+  ...baseConfig,
   {
     files: ["*.{js,ts}"],
     ignores: ["apps/**", "packages/**"],
-
-    languageOptions: {
-      parser: tseslint.parser,
-      parserOptions: {
-        project: true,
-        tsconfigRootDir: __dirname,
-      },
-    },
-
-    // ...workspaceLibrary,
   },
 ];
